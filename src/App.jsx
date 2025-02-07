4b2bfb84800d5aa5f6e47bb45a7b20e4430d27f7
Naveed03010408761 <akmal030104087@gmail.com>
2025-02-07 09:26:56 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './index.css';
+import './App.css';
+function App() {
+  return (
+    <div className="bg-blue-500 min-h-screen">
+      <div className="bg-blue-500 text-white p-10">
+        <h1 className="bg-red-500 text-white p-5">
+          App
+        </h1>
+      </div>
+    </div>
+  );
+}
+export default App;
